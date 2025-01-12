84e618b1fefcdae6f891ee3e6f9c9e6312998f05
towhidulbu08 <towhidulislam26121997@gmail.com>
2025-01-12 14:46:49 +0600

    L-9.1 Data Fetching - on the server with "fetch" added

--- app/api/posts/route.js
+++ app/api/posts/route.js
@@ -1,9 +1,9 @@
 import getJoke from "@/utils/getJoke";
 
-export const dynamic = "force-dynamic";
+//export const dynamic = "force-dynamic";
 
 export async function GET(request) {
-    const posts = await getJoke();
+  const posts = await getJoke();
 
-    return Response.json(posts);
+  return Response.json(posts);
 }
--- utils/getJoke.js
+++ utils/getJoke.js
@@ -1,13 +1,13 @@
 export default async function getJoke() {
-    const res = await fetch("https://api.chucknorris.io/jokes/random", {
-        next: {
-            tags: ["get-joke"],
-        },
-    });
+  const res = await fetch("https://api.chucknorris.io/jokes/random", {
+    next: {
+      tags: ["get-joke"],
+    },
+  });
 
-    if (!res.ok) {
-        throw new Error("Fetch error...");
-    }
+  if (!res.ok) {
+    throw new Error("Fetch error...");
+  }
 
-    return res.json();
+  return res.json();
 }
